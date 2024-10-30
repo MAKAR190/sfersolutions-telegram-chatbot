5afb7fbabea9cfd0b36b15309f651275b990fbbb
MAKAR190 <lutskyimakar@gmail.com>
2024-10-30 15:03:34 +0100

    pushed

--- config/config.js
+++ config/config.js
@@ -3,5 +3,8 @@ require("dotenv").config();
 module.exports = {
   TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN,
   GOOGLE_SHEET_ID: process.env.GOOGLE_SHEET_ID,
-  GOOGLE_CREDENTIALS: require("../sfersolutions-7cf634c130f3.json"),
+  GOOGLE_CREDENTIALS: {
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"), 
+  },
 };
